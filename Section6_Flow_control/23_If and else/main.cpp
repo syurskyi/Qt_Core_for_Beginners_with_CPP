17f248818a77e45b43b9cf1b30838f9dadd37d6c
Grey <sergejyurskyj@yahoo.com>
2020-07-25 15:58:52 -0700

    Committed 23_If and else

---
+++ Section6_Flow_control/23_If and else/main.cpp
@@ -0,0 +1,30 @@
+//
+// Created by syurskyi on 7/25/2020.
+//
+
+#include <QCoreApplication>
+
+#include <iostream>
+#include <QDebug>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int age;
+    qInfo() << "Enter your age:";
+    cin >> age;
+
+    if(age > 17) {
+        qInfo() << "You are over 17 years old!";
+    } else if(age < 12){
+        qInfo() << "You are not a teenager yet!";
+    } else {
+        qInfo() << "You are not over 17!";
+    }
+
+    return a.exec();
+}
+
