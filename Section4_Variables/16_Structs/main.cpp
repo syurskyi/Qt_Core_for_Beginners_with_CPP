f8a401d7a98fc13b25c89ed47c13863e8a89ebce
Grey <sergejyurskyj@yahoo.com>
2020-07-25 15:47:28 -0700

    Committed 16_Structs

---
+++ Section4_Variables/16_Structs/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by syurskyi on 7/25/2020.
+//
+
+#include <QCoreApplication>
+#include <QDebug>
+
+enum Colors {black, red, green, blue};
+
+//Precusor to "classes"
+struct product
+{
+    int weight;
+    double value;
+    Colors color;
+};
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    product laptop; // no assignment!!!
+    qInfo() << "Size of product: " << sizeof (laptop);
+
+    laptop.color = Colors::green;
+    laptop.value = 550.37;
+    laptop.weight = 3;
+
+    qInfo() << "Weight: " << laptop.weight;
+    qInfo() << "Value: " << laptop.value;
+    qInfo() << "Color: " << laptop.color;
+    //qInfo() << "Laptop: " << laptop;
+    return a.exec();
+}
