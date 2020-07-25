f9e528fb3c028a74f25820a342d741b612f7274d
Grey <sergejyurskyj@yahoo.com>
2020-07-25 16:09:59 -0700

    Committed 33. Functions in structs

--- Section7_Functions/33. Functions in structs/main.cpp
+++ Section7_Functions/33. Functions in structs/main.cpp
@@ -2,3 +2,27 @@
 // Created by syurskyi on 7/25/2020.
 //
 
+#include <QCoreApplication>
+#include <QDebug>
+
+struct laptop {
+    int weight;
+
+    double asKilograms() {
+        return weight * 0.453592;
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    laptop notebook;
+    notebook.weight = 5;
+
+    qInfo() << "Pounds: " << notebook.weight;
+    qInfo() << "Kilograms: " << notebook.asKilograms();
+
+    return a.exec();
+}
