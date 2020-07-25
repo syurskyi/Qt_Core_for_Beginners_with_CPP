05b7661bd5e36a7ebb6a24343046d2c8187945f0
Grey <sergejyurskyj@yahoo.com>
2020-07-25 16:05:20 -0700

    Committed 29_Function params

--- Section7_Functions/29_Function params/main.cpp
+++ Section7_Functions/29_Function params/main.cpp
@@ -2,3 +2,35 @@
 // Created by syurskyi on 7/25/2020.
 //
 
+#include <QCoreApplication>
+#include <QDebug>
+#include <iostream>
+
+using namespace std;
+
+int calc(int offset, int age) {
+    return offset * age;
+}
+
+int catYears(int age) {
+    return calc(9,age);
+}
+
+int dogYears(int age) {
+    return calc(7,age);
+}
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int age;
+    qInfo() << "Enter your age:";
+    cin >> age;
+
+    qInfo() << "You are " << dogYears(age) << " in dog years";
+    qInfo() << "You are " << catYears(age) << " in cat years";
+    return a.exec();
+}
+
