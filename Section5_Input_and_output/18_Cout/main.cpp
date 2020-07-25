cff8bcd3721d0c1a29ab11928c71318d8ffcd01a
Grey <sergejyurskyj@yahoo.com>
2020-07-25 15:50:00 -0700

    Committed 18_Cout

---
+++ Section5_Input_and_output/18_Cout/main.cpp
@@ -0,0 +1,25 @@
+//
+// Created by syurskyi on 7/25/2020.
+//
+
+#include <QCoreApplication>
+
+#include <iostream>
+#include <QDebug>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    cout << "Hello" << endl;
+    qInfo() << "hello";
+
+    int age = 44;
+    cout << age;
+    cout << endl;
+    cout << flush;
+
+    return a.exec();
+}
+
