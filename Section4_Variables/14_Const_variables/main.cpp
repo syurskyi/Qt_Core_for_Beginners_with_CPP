75b4d3ecd17d1f1496c774d3d5c9575846b0ff88
Grey <sergejyurskyj@yahoo.com>
2020-07-25 15:43:05 -0700

    Committed 14_Const_variables

---
+++ Section4_Variables/14_Const_variables/main.cpp
@@ -0,0 +1,20 @@
+//
+// Created by syurskyi on 7/25/2020.
+//
+
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int age = 44; //can change
+    const int id = 215; //can not change
+
+    id = 1112;
+    qInfo() << "Id: " << id;
+
+    return a.exec();
+}
+
---
+++ Section4_Variables/15_Enums/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by syurskyi on 7/25/2020.
+//
+
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    //enums
+
+    //enum Color {red, green, blue}; //zero based
+    enum Color {red = 100, green  = 56, blue = 8123}; //zero based
+    Color mycolor = Color::blue;
+    qInfo() << "Color: " << mycolor;
+
+    return a.exec();
+}
+
