7436abda814035d3278b2f85b5c6cd35a18be773
Grey <sergejyurskyj@yahoo.com>
2020-07-25 16:02:06 -0700

    Committed 26_Do while

--- Section6_Flow_control/26_Do while/main.cpp
+++ Section6_Flow_control/26_Do while/main.cpp
@@ -2,3 +2,35 @@
 // Created by syurskyi on 7/25/2020.
 //
 
+#include <QCoreApplication>
+
+#include <iostream>
+#include <QDebug>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int start = 80;
+    int max = 10;
+
+    int i  = start;
+    while (i < max) {
+        qInfo() << "WHILE = " << i;
+        i++;
+    }
+
+    qInfo("");
+    i = start;
+    do {
+        qInfo() << "DO = " << i;
+        i++;
+    } while (i < max);
+
+    qInfo() << "Finished";
+
+    return a.exec();
+}
+
